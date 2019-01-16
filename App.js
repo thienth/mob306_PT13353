921392139b6b9e6b4a918e58dbaa70659de49501
thienth32 <thienth32@gmail.com>
2019-01-16 09:45:21 +0700

    update list view

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ListView } from 'react-native';
 import TestCom from './src/components/TestCom';
 export default class App extends React.Component {
   constructor(props){
     super(props);
-    
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       multiple: 1,
       username: 'thienth',
       email: 'thienth32@gmail.com',
-      posts: [
+
+      posts: ds.cloneWithRows([
         {
           id: 1,
           title: "Đề cử top 100 gương mặt đẹp trai nhất châu Á: Nhân vật gây sốc nhất lại không thuộc showbiz Hoa ngữ",
@@ -22,43 +23,22 @@ export default class App extends React.Component {
           img: 'https://kenh14cdn.com/zoom/280_175/2019/1/10/ava-ngang-15471083072941271999927-crop-1547108727298370386490.png',
           shortDesc: "Ngày 10/1, danh sách đề cử top 100 gương mặt đẹp trai nhất Châu Á đã chính thức được tiết lộ với nhiều tên tuổi trong showbiz Hoa ngữ."
         }
-      ]
+      ])
     };
   }
 
   render() {
-    var items = [];
-    for(let i = 0; i < this.state.posts.length; i++){
-      let postItem = this.state.posts[i];
-      items.push(
-        <TestCom key={i} content={postItem}></TestCom>
-      )
-    }
+    
     return (
-      <View style={styles.container}>
-        <TextInput 
-          style={{
-            borderColor: "red", 
-            borderStyle: 'solid', 
-            borderWidth: 1,
-            width: '100%',
-            height: 40
-          }}
-          onChangeText={(text) => this.setState({multiple: text})}
+        <ListView
+          dataSource={this.state.posts}
+          renderRow={(r) => 
+            <View>
+              <Image source={{uri: r.img}} style={{width: 100, height: 100}}/>
+              <Text>{r.title}</Text>
+            </View>
+          }
         />
-        <TextInput 
-          style={{
-            borderColor: "red", 
-            borderStyle: 'solid', 
-            borderWidth: 1,
-            width: '100%',
-            height: 40
-          }}
-          onChangeText={(text) => this.setState({username: text})}
-        />
-        {items}
-        
-      </View>
     );
   }
 }
