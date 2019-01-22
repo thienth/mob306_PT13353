5b76712c2ccc996804c75abd99993891c2e3785b
thienth32 <thienth32@gmail.com>
2019-01-22 07:56:28 +0700

    update

--- App.js
+++ App.js
@@ -1,164 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
-import TestCom from './src/components/TestCom';
-import firebaseConf from './src/helpers/firebase';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
-    this.saveNew = this.saveNew.bind(this);
-    this.getWeather = this.getWeather.bind(this);
-
-    this.state = {
-      weatherApiKey: "104f298a8d04f8f4c48d0553479b3b7e",
-      cityWeatherApi: "http://api.openweathermap.org/data/2.5/weather?appid=104f298a8d04f8f4c48d0553479b3b7e&units=metric&q=",
-      defaultCity: "hanoi",
-      weatherObj: null,
-      multiple: 1,
-      username: 'thienth',
-      email: 'thienth32@gmail.com',
-      tmpid: "0",
-      tmptitle: "",
-      tmpimg: "",
-      tmpshortDesc: "",
-      posts: []
-    };
+    
   }
 
   componentDidMount() {
-    var that = this;
-    this.getWeather()
-
-    firebaseConf.database().ref('posts/').once('value', function (snapshot) {
-        let posts = [];
-        snapshot.forEach((child) => {
-
-          let item = {
-            key: child.key,
-            data: child.val()
-          }
-          posts.push(item);
-        });
-
-        that.setState({posts});
-        // console.log(that.state.posts);
-    });
+    
   }
   
-  getWeather(){
-
-    fetch(this.state.cityWeatherApi + this.state.defaultCity, {
-      method: "GET"
-    })
-    .then((res) => res.json())
-    .then(data => {
-      // console.log(data);
-      this.setState({weatherObj: data});
-    })
-  }
-  
-
-  saveNew(){
-    console.log(this.state.tmpid, this.state.tmptitle,
-      this.state.tmpimg, this.state.tmpshortDesc);
-    let newPost = {
-      id: this.state.tmpid,
-      title: this.state.tmptitle,
-      img: this.state.tmpimg,
-      shortDesc: this.state.tmpshortDesc,
-    };
-    let tmpPosts = [...this.state.posts];
-    tmpPosts.push(newPost);
-    this.setState({
-      posts: tmpPosts,
-      tmpid: "0",
-      tmptitle: "",
-      tmpimg: "",
-      tmpshortDesc: "",
-    });
-  }
 
   render() {
-    var weatherOutput = <Text>Loading</Text>;
-    if(this.state.weatherObj != null){
-      weatherOutput = (
-        <View>
-          <Text>Thành phố: {this.state.weatherObj.name}, {this.state.weatherObj.sys.country}</Text>
-          <Text>Nhiệt độ: {this.state.weatherObj.main.temp} độ C</Text>
-          <Text>Độ ẩm: {this.state.weatherObj.main.humidity}%</Text>
-          <Text>Tầm nhìn xa: {this.state.weatherObj.visibility/1000} km</Text>
-        </View>
-      );
-    }
+    
     
     return (
-       <ScrollView style={{marginTop: 50}}>
-         <Text>ID bài viết mới</Text>
-         <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(txt) => {this.setState({tmpid: txt})}}
-          value={`${this.state.tmpid}`}
-        />
-        <Text>Tiêu đề bài viết </Text>
-         <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(txt) => {this.setState({tmptitle: txt})}}
-          value={this.state.tmptitle}
-        />
-
-        <Text>Ảnh đại diện </Text>
-         <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(txt) => {this.setState({tmpimg: txt})}}
-          value={this.state.tmpimg}
-        />
-
-        <Text>Mô tả ngắn </Text>
-         <TextInput
-          style={{height: 100, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(txt) => {this.setState({tmpshortDesc: txt})}}
-          value={this.state.tmpshortDesc}
-          multiline={true}
-        />
-        <TouchableOpacity
-         style={styles.button}
-         onPress={this.saveNew}
-        >
-          <Text>Thêm mới</Text>
-        </TouchableOpacity>
-          <TextInput
-            value={this.state.defaultCity}
-            onChangeText={txt => {this.setState({defaultCity : txt})}}
-          />
-          <TouchableOpacity
-          style={styles.button}
-          onPress={this.getWeather}
-          >
-            <Text>Tìm kiếm</Text>
-          </TouchableOpacity>
-          {weatherOutput}
-          
-         {this.state.posts.map(row => 
-            <View key={row.key}>
-              <Image source={{uri: row.data.image}} style={{width: 100, height: 100}}/>
-              <Text>{row.data.name}</Text>
-            </View>
-          )}
-       </ScrollView>
+      <View style={styles.mainContainer}>
+          <View style={styles.item}>
+            <View style={styles.itemImg}></View>
+            <View style={styles.itemContent}></View>
+          </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer:{
+    marginTop: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10
+  item:{
+    height: 150,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    flexDirection: 'row'
   },
+  itemImg:{
+    flex:2,
+    borderRightColor: '#ccc',
+    borderRightWidth: 1,
+    backgroundColor: "green"
+  },
+  itemContent:{
+    flex:3,
+    backgroundColor: "blue"
+  }
 });
